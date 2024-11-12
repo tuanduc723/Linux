1a86957bc6a0fc6aeef18b5bc000cd629573f3b5
tuanduc723 <duc.2274802010191@vanlanguni.vn>
2024-11-12 11:55:30 +0700

    Create train_classifier.py

---
+++ train_classifier.py
@@ -0,0 +1,33 @@
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+def main():
+    data_dict = pickle.load(open('./data.pickle', 'rb'))
+
+    data = np.asarray(data_dict['data'])
+    labels = np.asarray(data_dict['labels'])
+
+    if len(data) == 0 or len(labels) == 0:
+        print("No data available. Please ensure you have collected and created the dataset correctly.")
+        return
+
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+    model = RandomForestClassifier()
+
+    model.fit(x_train, y_train)
+
+    y_predict = model.predict(x_test)
+
+    score = accuracy_score(y_predict, y_test)
+
+    print('{}% of samples were classified correctly !'.format(score * 100))
+
+    with open('model.p', 'wb') as f:
+        pickle.dump({'model': model}, f)
+
+if __name__ == "__main__":
+    main()
